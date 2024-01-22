402f0b2fa709f386c49f136970886cd974223b4c
UnknownGamer925 <137815335+UnknownGamer925@users.noreply.github.com>
2024-01-22 16:35:13 +0000

    22/01/24 Audience Animation

---
+++ Assets/StageSceneContent/audience_cheer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audience_cheer : MonoBehaviour
+{
+    //Variables
+    private bool on_floor = false;
+    private float jump_height = 0.0f;
+    public bool laugh_active = true;
+
+    void Laugh()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector3(0, jump_height, 0);
+        
+     
+    }
+
+    private void Randomiser()
+    {
+        jump_height = Random.Range(10f, 20f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponentInParent<AudienceControl>().AudienceTrigger == true & on_floor == true)
+        {
+            on_floor = false;
+            Randomiser();
+            Laugh();
+        }
+      
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            on_floor = true;
+        }
+
+        
+    }
+
+}
