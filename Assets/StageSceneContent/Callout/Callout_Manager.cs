d2b4cfb2118066b0fe5c04793a8c5fdb9489f578
UnknownGamer925 <joemcd.business@gmail.com>
2024-01-24 18:06:16 +0000

    24/01/24 Callouts updated
    Callouts now fully functional

--- Assets/StageSceneContent/Callout/Callout_Manager.cs
+++ Assets/StageSceneContent/Callout/Callout_Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Callout_Manager : MonoBehaviour
@@ -9,53 +10,55 @@ public class Callout_Manager : MonoBehaviour
     public bool Good = false;
     public bool Mid = false;
     public bool Bad = false;
-    
+    private int control = 0;
+    private int num = 0;
+
+
+    private void Reset()
+    {
+        Excellent = false;
+        Good = false;
+        Mid = false;
+        Bad = false;
+    }
+
+    void PickChild()
+    {
+        if (Excellent == true) { num = 0; }
+        else if (Good == true) { num = 1; }
+        else if (Mid == true) { num = 2; }
+        else if (Bad == true) { num = 3; }
+    }
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.transform.GetChild(3).GetComponent<SpriteRenderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Excellent == true)
+        PickChild();
+        if (Excellent == true || Good == true || Mid == true || Bad == true)
         {
-            gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
-            gameObject.transform.GetChild(0).GetComponent<Animator>().ResetTrigger("Callout_anim");
-        }
-        else
-        {
-            gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
-            gameObject.transform.GetChild(0).GetComponent<Animator>().enabled = false;
-        }
+            gameObject.transform.GetChild(num).GetComponent<SpriteRenderer>().enabled = true;
+            if (control == 0)
+            {
+                gameObject.transform.GetChild(num).GetComponent<Animation>().Play();
+                control = 1;
+                Invoke("Reset", 1.4f);
+            }
 
-        if (Good == true)
-        {
-            gameObject.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = true;
         }
         else
         {
-            gameObject.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
-        }
+            gameObject.transform.GetChild(num).GetComponent<SpriteRenderer>().enabled = false;
+            control = 0;
 
-        if (Mid == true)
-        {
-            gameObject.transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = true;
-        }
-        else
-        {
-            gameObject.transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = false;
-        }
-
-        if (Bad == true)
-        {
-            gameObject.transform.GetChild(3).GetComponent<SpriteRenderer>().enabled = true;
-        }
-        else
-        {
-            gameObject.transform.GetChild(3).GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 }
